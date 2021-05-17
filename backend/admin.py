57a9457b1b1b5b9715b87ee56ec134c9bf957a09
Ali Hamza Mohammed <alihamzamohammed2@gmail.com>
2021-05-17 20:55:08 +0100

    Created first models

--- backend/admin.py
+++ backend/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Device, Network
 
 # Register your models here.
+
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'mac_address', 'network')
+
+class NetworkAdmin(admin.ModelAdmin):
+    list_display = ('name', 'broadcast_address')
+
+admin.site.register(Device, DeviceAdmin)
+admin.site.register(Network, NetworkAdmin)
--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.db.models.fields.related import ForeignKey
 
 # Create your models here.
+
+class Network(models.Model):
+    name = models.CharField(max_length=128)
+    broadcast_address = models.CharField(max_length=16)
+
+    def __str__(self):
+        return self.name
+
+class Device(models.Model):
+    name = models.CharField(max_length=128)
+    mac_address = models.CharField(max_length=16)
+    network = models.ForeignKey(Network, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
