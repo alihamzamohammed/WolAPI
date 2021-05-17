30c6912b75c5da70bf18a19245faa82aff8532d6
Ali Hamza Mohammed <alihamzamohammed2@gmail.com>
2021-05-17 21:23:01 +0100

    Created views

--- backend/views.py
+++ backend/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import DeviceSerializer, NetworkSerializer
+from .models import Device, Network
 
 # Create your views here.
+
+class DeviceView(viewsets.ModelViewSet):
+    serializer_class = DeviceSerializer
+    queryset = Device.objects.all()
+
+class NetworkView(viewsets.ModelViewSet):
+    serializer_class = NetworkSerializer
+    queryset = Network.objects.all()
